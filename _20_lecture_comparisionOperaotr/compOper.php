c53bbfed179f2cec345cfc9a186eb5df9ab884ed
Prince kumar <princekumar7320918928@gmail.com>
2024-08-15 19:16:31 +0530

    20 Comparison Operators in PHP

---
+++ _20_lecture_comparisionOperaotr/compOper.php
@@ -0,0 +1,25 @@
+<!-- if else in php -->
+
+<?php
+$a=10;
+$b=20;
+if($a>$b){
+  echo "$a is greater than $b";
+}
+else{
+  echo "$b is greater than $a";
+}
+
+echo "<br/>";
+echo $a<>$b;
+
+//different comparision operator
+// ==
+// !=
+// <
+// >
+// <=
+// >=
+// ===
+// !==
+// <>

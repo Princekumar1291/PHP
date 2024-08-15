92dd29c41cb18652de9773d9c9af75a8372fc7e3
Prince kumar <princekumar7320918928@gmail.com>
2024-08-15 16:56:05 +0530

    13 Constants in PHP

---
+++ _13_lecture_constant/constant.php
@@ -0,0 +1,28 @@
+<?php
+// $data="Prince Kumar";
+// echo $data;
+
+const data2="Maurya ji";
+// data2="Maurya ji ji";   not possible
+echo data2;
+
+?>
+
+
+<?php
+
+$name="prince kumar";
+$name="tonny";
+echo $name;
+
+?>
+
+
+<?php
+
+define("myData","Anil shidhu");
+echo myData;
+
+?>
+
+

3131e5cb9f22fc62cb5c0c0640abb153aeb45300
Prince kumar <princekumar7320918928@gmail.com>
2024-08-15 17:28:47 +0530

    15  comments

---
+++ _15_lecture_comments/comments.php
@@ -0,0 +1,13 @@
+<?php
+// this is comment in php
+
+# this is comment in php
+
+
+/*
+this is multiline comment in php
+ha ye comment hai
+*/
+
+$user="Prince Kumar";  #this is user name
+$age==20; //this is age of user

5bab5430c8b316cacbf2558ab81749e58f70e0f6
Prince kumar <princekumar7320918928@gmail.com>
2024-08-15 17:59:55 +0530

    19 Assignment Operators in PHP

---
+++ _19_lecture_assignmentOperator/assignment.php
@@ -0,0 +1,19 @@
+<?php
+// assignment operaotr 
+
+$a = 10;
+$b = 20;
+$b=$a;
+echo $b;
+echo "<br/>";
+
+echo $a;
+echo "<br/>";
+
+// we can use these operators in assigment
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=

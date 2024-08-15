1bcf138bb02436bd9b19eb58ef1173ac3142f782
Prince kumar <princekumar7320918928@gmail.com>
2024-08-15 17:53:10 +0530

    18 Arithmetic operators in PHP

---
+++ _18_lecture_arithmeticOperaor/arithOpr.php
@@ -0,0 +1,29 @@
+<!--arithmetic operator expmle -->
+<?php
+
+$a=10;
+$b=5;
+echo $a+$b;
+echo "<br>";
+
+echo $a-$b;
+echo "<br>";
+
+echo $a*$b;
+echo "<br>";
+
+echo $a/$b;
+echo "<br>";
+
+echo $a%$b;
+echo "<br>";
+
+echo $a**$b;
+echo "<br>";
+
+const num1=10;
+const num2= 20;
+const num3= 30;
+echo num1+num2+num3;
+echo "<br>";
+

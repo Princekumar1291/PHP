4fbbcc0936299758832b7a062dba03c2e942652b
Prince kumar <princekumar7320918928@gmail.com>
2024-08-16 08:22:35 +0530

    21 Increment / Decrement

---
+++ _21_lecture_incrementDecrement/incrementDecrement.php
@@ -0,0 +1,24 @@
+<?php
+$age=21;
+$age++;
+echo $age;
+echo "<br>";
+
+$age--;
+echo $age;
+echo "<br>";
+
+$cont=1000;
+echo ++$cont;
+echo "<br>";
+
+echo --$cont;
+echo "<br>";
+
+echo $cont++;
+echo "<br>";
+
+echo $cont--;
+echo "<br>";
+
+?>

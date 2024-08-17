54f56b0589c17322a9bc21fc0dc28b2096eff6f0
Prince kumar <princekumar7320918928@gmail.com>
2024-08-17 12:41:57 +0530

    array basic pop push splice

--- _43_lecture_array/array.php
+++ _43_lecture_array/array.php
@@ -1,12 +1,15 @@
 <?php
 $user=["Prince Kumar",20];
-echo $user[0];
-echo "<br>";
-echo $user[1];
-echo "<br>";
+// echo $user[0];
+// echo "<br>";
+// echo $user[1];
+// echo "<br>";
 
 $arr=["Ram","Shayam","Abhishek","Prakhar","Anil"];
 array_push($arr,"Mohit");
+array_push($arr,"Mohit","Anil","Prakhar");
+array_pop($arr);
+array_splice($arr,-2);
 
 for($i=0;$i<sizeof($arr);$i++){
   echo $arr[$i];

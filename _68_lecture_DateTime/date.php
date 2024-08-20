dc2bde917a476f9759419d12e0f147752f891714
Prince kumar <princekumar7320918928@gmail.com>
2024-08-20 16:45:50 +0530

    68 Date and Time

---
+++ _68_lecture_DateTime/date.php
@@ -0,0 +1,44 @@
+<?php
+echo date("d-m-Y");
+echo "</br>";
+
+echo date("D-M-Y");
+echo "</br>";
+
+echo date('d');
+echo "</br>";
+
+echo date('D');
+echo "</br>";
+
+echo date('l');
+echo "</br>";
+echo "</br>";
+
+echo date('m');
+echo "</br>"; 
+
+echo date('M');
+echo "</br>";
+echo "</br>";
+
+echo date('y');
+echo "</br>";
+
+echo date('Y');
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
+// time property 
+
+echo time();
+echo "</br>";
+
+
+date_default_timezone_set('Asia/Kolkata');
+echo  date("h:i:s A");
+echo "</br>";
+
+echo date("H:i:s A");
+echo "</br>";

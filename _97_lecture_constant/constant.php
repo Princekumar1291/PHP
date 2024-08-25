b42c4bc3f1a33611b8f0de7a1408f807b9fe2222
Prince kumar <princekumar7320918928@gmail.com>
2024-08-25 08:06:35 +0530

    Constant in PHP class

--- _97_lecture_constant/constant.php
+++ _97_lecture_constant/constant.php
@@ -13,3 +13,8 @@ echo "<br>";
 
 $c1=new constantDemo();
 $c1->getColledgeName();
+
+//:: (Scope Resolution Operator):
+
+// The scope resolution operator :: is used to access class members (like constants, methods, or static properties) directly from the class name rather than from an instance of the class.
+// It's often referred to as the "double colon" or "Paamayim Nekudotayim" in PHP.
